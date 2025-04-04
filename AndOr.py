22366a4f104f78ccef992738c7e50dbd7a26f81b
NatalieZawadi <nataliezawadiwesula@gmail.com>
2025-04-04 19:26:17 +0300

    And Or operators

---
+++ AndOr.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:
+    print("Atleast one number has boolean value as False")
+
+A = 10
+B = -10
+C = 0
+
+if A > 0 or B > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if B > 0 or C > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
